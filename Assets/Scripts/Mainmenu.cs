cc140b7a9bab8e6af0e6f2304c8fecc1dd13ca30
Lee-Kyung-Sup(이경섭) <120997897+Lee-Kyung-Sup@users.noreply.github.com>
2024-02-07 08:49:56 +0900

    UI practice

---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,77 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScreen : MonoBehaviour
+{
+    public TextMeshProUGUI BankMoneyText;
+    public TextMeshProUGUI MoneyText;
+    public TMP_InputField amountInput;
+    public Button DepositButton;
+    public Button withdrawButton;
+
+    private float BankMoney = 100000;
+    private float Money = 50000;
+
+    private void Start()
+    {
+        UpdateMoneyText();
+        UpdateBankMoneyText();
+    }
+
+    //입금
+    public void Deposit()
+    {
+        if (float.TryParse(amountInput.text, out float depositAmount))
+        {
+            if (depositAmount <= Money)
+            {
+                Money -= depositAmount;
+                BankMoney += depositAmount;
+                UpdateMoneyText();
+                UpdateBankMoneyText();
+            }
+            else
+            {
+                Debug.Log("Not Enough Money");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid Input for Deposit");
+        }
+    }
+
+    //출금
+    public void Withdraw()
+    {
+        if (float.TryParse(amountInput.text, out float withdrawAmount))
+        {
+            if (withdrawAmount <= BankMoney)
+            {
+                Money += withdrawAmount;
+                BankMoney -= withdrawAmount;
+                UpdateMoneyText();
+                UpdateBankMoneyText();
+            }
+            else
+            {
+                Debug.Log("Not Enough Money in the Bank");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid Input for Withdraw");
+        }
+    }
+
+    public void UpdateBankMoneyText()
+    {
+        BankMoneyText.text = BankMoney.ToString("C2");
+    }
+
+    public void UpdateMoneyText()
+    {
+        MoneyText.text = Money.ToString("C2");
+    }
+}
